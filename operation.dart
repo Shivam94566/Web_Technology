6e54140ffe7c6ca61bb22d60d05a192c14d0762f
Shivam Bhardwaj <shivambhardwaj.94566@gmail.com>
2026-02-25 09:32:27 +0530

    update existing

--- operation.dart
+++ operation.dart
@@ -1,7 +1,42 @@
-import 'dart:io';
+// void greet(){
+//   print("Hello,Dart!");
 
-void main() {
-  print("Enter your age");
-  int age = int.parse(stdin.readLineSync()!);
-  print("your age is $age");
+// }
+// void main(){
+//   greet();
+// }
+
+
+
+// int add (int a , int b){
+//   return a +b;
+
+// }
+// void main(){
+//   int result=add(10,20);
+//   print(result);
+// }
+
+
+///positional optional parameter ////
+
+// Use {}
+// void greet(String name , [String? title]){
+//   print("Hello $title $name");
+
+// }
+// Calling:
+// greet("Abhishek");
+// greet("Abhishek", "Dr.");
+git 
+
+
+//Named optional parameter 
+
+Use {}
+void greet ({String? name, int? age}){
+  print("Name: $name , Age: $age");
+
 }
+Calling:
+greet(name: "Abhishek", age: 35);
